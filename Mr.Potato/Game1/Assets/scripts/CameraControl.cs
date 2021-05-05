3793f1aae14689951464e645fd7e8af826871579
cyberjokerlove <2676087334@qq.com>
2021-05-05 17:41:00 +0800

    第二次实验

---
+++ Mr.Potato/Game1/Assets/scripts/CameraControl.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraControl : MonoBehaviour
+{
+    Transform player;
+    public float xMargin = 10.0f;
+    public float yMargin = 10.0f;
+    public float SmoothX = 1.0f;
+    public float SmoothY = 1.0f;
+    public float MaxCamX = 5.0f;
+    public float MaxCamY = 4.0f;
+    public float MinCamX = -5.0f;
+    public float MinCamY = -1.0f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void Awake() {
+        player = GameObject.FindGameObjectWithTag("Player").transform;
+    }
+    bool NeedMovex()
+    {
+        return Mathf.Abs(transform.position.x - player.position.x) > xMargin;
+    }
+    bool NeedMoveY()
+    {
+        return Mathf.Abs(transform.position. y- player.position.y) > yMargin;
+    }
+
+    private void FixedUpdate()
+    {
+        TrackPlayer();
+    } 
+
+    void TrackPlayer() {
+    float CamNewX = transform.position.x;
+    float CamNewY = transform.position.y;
+    if (NeedMovex())//计算新摄像机的位置
+        CamNewX = Mathf.Lerp(transform.position.x, player.position.x, SmoothX * Time.deltaTime);
+    if (NeedMoveY())
+        CamNewY = Mathf.Lerp(transform.position.y, player.position.y, SmoothY * Time.deltaTime);
+    //将摄像机位置固定在合法范围内
+    CamNewX = Mathf.Clamp(CamNewX,MinCamX, MaxCamX);
+    CamNewY = Mathf.Clamp(CamNewY,MinCamX, MaxCamX);
+        transform.position = new Vector3(CamNewX, CamNewY, transform.position.z);
+}
+}
---
+++ Mr.Potato/Game1/Assets/scripts/PlayerControl.cs
@@ -0,0 +1,71 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControl : MonoBehaviour
+{
+    // Start is called before the first frame update
+    private Rigidbody2D heroBody;
+    public float moveForce = 100;
+    public float jumpForce = 500;
+    private float fInput = 0.0f;
+    public float maxSpeed = 5;
+    private bool bFaceRight = true;
+    private bool bJump = false;
+    private bool bGrounded = false;
+    Transform mGroundcheck;
+
+    void Start()
+    {
+        heroBody = GetComponent<Rigidbody2D>();
+        mGroundcheck = transform.Find("GroundCheck");//找到空物体的位置
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        fInput = Input.GetAxis("Horizontal");
+        if (fInput < 0 && bFaceRight)
+            flip();
+        else if (fInput >0 && !bFaceRight)
+            flip();
+
+        heroBody.AddForce(Vector2.right * fInput * moveForce);
+
+        bGrounded=Physics2D.Linecast(transform.position, mGroundcheck.position, 1 << LayerMask.NameToLayer("Ground"));//判断空物体Groundcheck是否在地面上，是则返回true
+        if (bGrounded)//判断物体在地面上时是否按下空格
+        {
+            bJump = Input.GetKeyDown(KeyCode.Space);
+            Vector2 upForce = new Vector2(0, 1);
+            if (bJump)
+            {
+                heroBody.AddForce(upForce* jumpForce);
+                bJump = false;
+            }
+        }
+        
+    }
+
+    private void FixedUpdate() {
+
+        if (Mathf.Abs(heroBody.velocity.x) < maxSpeed)
+        //if (fInput * heroBody.velocity.x < MaxSpeed) ;
+           heroBody.AddForce(fInput * moveForce * Vector2.right);
+
+        if (Mathf.Abs(heroBody.velocity.x) > maxSpeed)
+            heroBody.velocity = new Vector2(Mathf.Sign(heroBody.velocity.x) * maxSpeed, heroBody.velocity.y);
+
+        /*if (bJump) {
+            heroBody.AddForce(new Vector2(0f, jumpForce));
+            bJump = false;
+        }*/
+    }
+
+    void flip() {
+        Vector3 theScale = transform.localScale;
+        theScale.x *= -1;
+        transform.localScale= theScale;
+        bFaceRight = !bFaceRight;
+    }
+}
